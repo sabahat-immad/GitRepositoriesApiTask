3fa3c03db1d937628736e9cab0a4f9f5798752e3
sabahat <sabahat.qazi.immad@gmail.com>
2020-08-28 13:44:00 +0100

    first commit

--- app/src/main/java/com/saba/gitrepotask/ui/GitRepoViewModel.kt
+++ app/src/main/java/com/saba/gitrepotask/ui/GitRepoViewModel.kt
@@ -6,25 +6,37 @@ import java.util.*
 import androidx.databinding.Observable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import com.saba.gitrepotask.data.model.Commits
 import com.saba.gitrepotask.data.model.GitRepositories
+import kotlinx.coroutines.launch
 import retrofit2.Response
 
 class GitRepoViewModel(private val gitRepoMVVMRepository: GitRepoMVVMRepository) : ViewModel(), Observable{
 
     fun getGitRepositories() : LiveData<Response<GitRepositories>>{
-        return liveData {
-            val response : Response<GitRepositories> = gitRepoMVVMRepository.getGitRepositories()
-            emit(response)
+        lateinit var data : LiveData<Response<GitRepositories>>
+        viewModelScope.launch {
+            data =  liveData {
+                val response : Response<GitRepositories> = gitRepoMVVMRepository.getGitRepositories()
+                emit(response)
+            }
+
         }
+        return data
     }
 
     fun getLastCommit(url : String) : LiveData<Response<Commits>>{
-        val newUrl = url.dropLast(6)
-        return liveData {
-            val response = gitRepoMVVMRepository.getLastCommit(newUrl)
-            emit(response)
+        lateinit var data : LiveData<Response<Commits>>
+        viewModelScope.launch {
+            val newUrl = url.dropLast(6)
+             data =  liveData {
+                val response = gitRepoMVVMRepository.getLastCommit(newUrl)
+                emit(response)
+            }
+
         }
+        return data
     }
     override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
 
